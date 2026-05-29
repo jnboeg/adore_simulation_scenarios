89a30417e36d3ecd878f1bf3371722a205675ec6
Jan Niklas Böger <jan.boeger@dlr.de>
2026-05-29 09:16:33 +0200

    edemissn scenario with straight line added
    Signed-off-by: Jan Niklas Böger <jan.boeger@dlr.de>

---
+++ edemissen_line.py
@@ -0,0 +1,45 @@
+# ********************************************************************************
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+# ********************************************************************************
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import os
+import sys
+sys.path.append(os.path.dirname(__file__)) # this line is very importatnt to find the helper functions
+
+from position import Position
+from simulated_vehicle import create_simulated_vehicle
+from visualizer import create_visualizer
+
+start_position = Position(lat_long=(52.402415, 10.22915), psi=0.0)
+goal_position = Position(lat_long=(52.402901, 10.231940), psi=0.0)
+
+def generate_launch_description():
+    return LaunchDescription([
+        *create_simulated_vehicle(
+            namespace="ego_vehicle",
+            # start_pose_utm=(583266.7740, 5806405.922, 2.0),
+            start_pose_utm=start_position.get_utm_coordinates(),
+            goal_position_utm=goal_position.get_utm_coordinates(),
+            v2x_id=0,
+            vehicle_id=111,
+            map_file="r2s_flightfield_edemissen_26022026_25832.r2sr",
+        ),
+        *create_visualizer(
+            whitelist=["ego_vehicle"],
+            # asset_folder=map_image_folder,
+            # visualization_offset=(606440.120, 5797321.700),
+            visualization_offset=start_position.get_utm_coordinates(),
+        ),
+    ])
